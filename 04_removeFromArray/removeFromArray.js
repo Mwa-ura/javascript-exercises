3bedaf1cf7f695802d10d728109887504d8d0b8b
Mwaura <101228726+Mwa-ura@users.noreply.github.com>
2023-11-30 03:42:18 +0400

    Solved first test.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, num) { 
+    var list = [];
+	for (i = 0; i<array.length; i++) {
+		var eachItem = array[i];
+		if (eachItem == num) { 
+			continue;
+		}
+		else {
+			list.push(eachItem)
+		}
+	}
+	return list;
 };
 
 // Do not edit below this line
