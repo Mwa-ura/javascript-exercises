249c9c58af60c59b6e380b5d4c281a79b993a57c
Mwaura <101228726+Mwa-ura@users.noreply.github.com>
2023-12-08 01:46:30 +0400

    Complete exercise 5.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
-
+const sumAll = function(firstNum, secondNum) {
+    let total = 0;
+    if (firstNum < 0) {
+        return 'ERROR';
+    }
+    else if (Number.isInteger(firstNum) === false) {
+        return 'ERROR';
+    }
+    else if (Number.isInteger(secondNum) === false) {
+        return 'ERROR';
+    }
+    else if (firstNum > secondNum) {
+        for (let i = secondNum; i<=firstNum; i++) {
+            total += i;
+        }
+        return total;
+    } 
+    else {
+        for (let i = firstNum; i<=secondNum; i++ ) {
+            total += i;
+        }
+        return total;
+    }
 };
 
 // Do not edit below this line
